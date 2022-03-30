0a2c9a54d8fee925304b82549714e7ca05abfea2
ss11mik <ss11mik@protonmail.com>
2022-03-30 14:51:16 +0200

    Add JSON library

---
+++ src/main/java/ija/ImportExport.java
@@ -0,0 +1,24 @@
+package ija;
+
+import ija.data_structures.*;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+public class ImportExport {
+
+    public static void save (UML_Diagram d) {
+        //System.out.println("aaa");
+/*
+        ObjectMapper mapper = new ObjectMapper();
+
+        String json = mapper.writeValueAsString(d);
+        System.out.println(json);*/
+    }
+
+
+    public static UML_Diagram load () {
+        return null;
+    }
+
+}

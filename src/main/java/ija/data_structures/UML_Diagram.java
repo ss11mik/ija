34bf0def575cbba69f093f2ab73e68e151f0bd34
ss11mik <ss11mik@protonmail.com>
2022-04-01 09:55:57 +0200

    Fix import/export

--- src/main/java/ija/data_structures/UML_Diagram.java
+++ src/main/java/ija/data_structures/UML_Diagram.java
@@ -3,6 +3,10 @@ package ija.data_structures;
 public class UML_Diagram {
     protected String name;
 
+    public UML_Diagram(){
+        // for JSON conversion
+    }
+
     public UML_Diagram(String name){
         this.name = name;
     }

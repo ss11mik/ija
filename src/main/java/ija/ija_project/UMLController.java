1ed9b9830af83c225e323458628a41d0c782c036
ss11mik <ss11mik@protonmail.com>
2022-04-08 08:20:42 +0200

    Add TabPane for diagrams

--- src/main/java/ija/ija_project/UMLController.java
+++ src/main/java/ija/ija_project/UMLController.java
@@ -6,18 +6,18 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
 public class UMLController {
-    @FXML
-    private Canvas canvas;
+//     @FXML
+//     private Canvas canvas;
 
     @FXML
     private void redrawCanvas() {
-        double w=canvas.getWidth();
-        double h=canvas.getHeight();
-        System.out.println(w);
-        GraphicsContext gc=canvas.getGraphicsContext2D();
-        gc.clearRect(0, 0, w, h);
-        gc.beginPath();
-        gc.rect(10, 10, w-200, h-20);
-        gc.stroke();
+//         double w=canvas.getWidth();
+//         double h=canvas.getHeight();
+//         System.out.println(w);
+//         GraphicsContext gc=canvas.getGraphicsContext2D();
+//         gc.clearRect(0, 0, w, h);
+//         gc.beginPath();
+//         gc.rect(10, 10, w-200, h-20);
+//         gc.stroke();
     }
 }
